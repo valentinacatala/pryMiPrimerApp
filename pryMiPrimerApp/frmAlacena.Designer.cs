c9d338524d9aed28eee3a70173b7e2475e5eaf29
Valentina Catalá <valenncatala@gmail.com>
2023-04-21 23:03:19 -0300

    21/04

--- pryMiPrimerApp/frmAlacena.Designer.cs
+++ pryMiPrimerApp/frmAlacena.Designer.cs
@@ -28,21 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAlacena));
             this.lblTipo = new System.Windows.Forms.Label();
             this.lblCantidad = new System.Windows.Forms.Label();
             this.lblNombre = new System.Windows.Forms.Label();
             this.lstTipo = new System.Windows.Forms.ComboBox();
             this.lstCantidad = new System.Windows.Forms.ComboBox();
             this.lstNombre = new System.Windows.Forms.ComboBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.chkCaduca = new System.Windows.Forms.CheckBox();
+            this.lstFechaDeVencimiento = new System.Windows.Forms.DateTimePicker();
             this.lblFechaDeVencimiento = new System.Windows.Forms.Label();
-            this.mrcCategoria = new System.Windows.Forms.GroupBox();
-            this.lblProducto = new System.Windows.Forms.Label();
+            this.mrcCategorias = new System.Windows.Forms.GroupBox();
             this.lstProducto = new System.Windows.Forms.ComboBox();
+            this.lblProducto = new System.Windows.Forms.Label();
             this.cmdGuardar = new System.Windows.Forms.Button();
             this.cmdCancelar = new System.Windows.Forms.Button();
-            this.mrcCategoria.SuspendLayout();
+            this.mrcCategorias.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTipo
@@ -50,9 +51,10 @@
             this.lblTipo.AutoSize = true;
             this.lblTipo.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTipo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblTipo.Location = new System.Drawing.Point(16, 61);
+            this.lblTipo.Location = new System.Drawing.Point(16, 102);
+            this.lblTipo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTipo.Name = "lblTipo";
-            this.lblTipo.Size = new System.Drawing.Size(30, 16);
+            this.lblTipo.Size = new System.Drawing.Size(41, 22);
             this.lblTipo.TabIndex = 1;
             this.lblTipo.Text = "Tipo";
             // 
@@ -61,9 +63,10 @@
             this.lblCantidad.AutoSize = true;
             this.lblCantidad.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCantidad.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblCantidad.Location = new System.Drawing.Point(16, 133);
+            this.lblCantidad.Location = new System.Drawing.Point(15, 206);
+            this.lblCantidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(55, 16);
+            this.lblCantidad.Size = new System.Drawing.Size(72, 22);
             this.lblCantidad.TabIndex = 2;
             this.lblCantidad.Text = "Cantidad";
             // 
@@ -72,9 +75,10 @@
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNombre.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblNombre.Location = new System.Drawing.Point(19, 99);
+            this.lblNombre.Location = new System.Drawing.Point(15, 157);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(48, 16);
+            this.lblNombre.Size = new System.Drawing.Size(64, 22);
             this.lblNombre.TabIndex = 3;
             this.lblNombre.Text = "Nombre";
             // 
@@ -82,9 +86,16 @@
             // 
             this.lstTipo.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstTipo.FormattingEnabled = true;
-            this.lstTipo.Location = new System.Drawing.Point(69, 58);
+            this.lstTipo.Items.AddRange(new object[] {
+            "Lacteo",
+            "Verdura",
+            "Fruta",
+            "Perecedero",
+            "Otro"});
+            this.lstTipo.Location = new System.Drawing.Point(97, 99);
+            this.lstTipo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstTipo.Name = "lstTipo";
-            this.lstTipo.Size = new System.Drawing.Size(121, 24);
+            this.lstTipo.Size = new System.Drawing.Size(160, 28);
             this.lstTipo.TabIndex = 4;
             this.lstTipo.SelectedIndexChanged += new System.EventHandler(this.lstTipo_SelectedIndexChanged);
             // 
@@ -92,124 +103,179 @@
             // 
             this.lstCantidad.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstCantidad.FormattingEnabled = true;
-            this.lstCantidad.Location = new System.Drawing.Point(80, 130);
+            this.lstCantidad.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.lstCantidad.Location = new System.Drawing.Point(97, 200);
+            this.lstCantidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstCantidad.Name = "lstCantidad";
-            this.lstCantidad.Size = new System.Drawing.Size(121, 24);
+            this.lstCantidad.Size = new System.Drawing.Size(160, 28);
             this.lstCantidad.TabIndex = 5;
             // 
             // lstNombre
             // 
             this.lstNombre.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstNombre.FormattingEnabled = true;
-            this.lstNombre.Location = new System.Drawing.Point(69, 91);
+            this.lstNombre.Items.AddRange(new object[] {
+            "Huevo",
+            "Leche",
+            "Queso",
+            "Naranja",
+            "Mandarina",
+            "Banana",
+            "Manzana",
+            "Frutilla",
+            "Lechuga",
+            "Tomate",
+            "Cebolla",
+            "Papa",
+            "Galleta",
+            "Arroz",
+            "Fideos",
+            "Otro"});
+            this.lstNombre.Location = new System.Drawing.Point(97, 151);
+            this.lstNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstNombre.Name = "lstNombre";
-            this.lstNombre.Size = new System.Drawing.Size(121, 24);
+            this.lstNombre.Size = new System.Drawing.Size(160, 28);
             this.lstNombre.TabIndex = 6;
             // 
-            // checkBox1
+            // chkCaduca
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.checkBox1.Location = new System.Drawing.Point(23, 171);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(66, 20);
-            this.checkBox1.TabIndex = 7;
-            this.checkBox1.Text = "Caduca";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.chkCaduca.AutoSize = true;
+            this.chkCaduca.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkCaduca.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.chkCaduca.Location = new System.Drawing.Point(19, 251);
+            this.chkCaduca.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chkCaduca.Name = "chkCaduca";
+            this.chkCaduca.Size = new System.Drawing.Size(84, 26);
+            this.chkCaduca.TabIndex = 7;
+            this.chkCaduca.Text = "Caduca";
+            this.chkCaduca.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // lstFechaDeVencimiento
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(142, 206);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker1.TabIndex = 8;
+            this.lstFechaDeVencimiento.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFechaDeVencimiento.Location = new System.Drawing.Point(175, 299);
+            this.lstFechaDeVencimiento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstFechaDeVencimiento.Name = "lstFechaDeVencimiento";
+            this.lstFechaDeVencimiento.Size = new System.Drawing.Size(265, 26);
+            this.lstFechaDeVencimiento.TabIndex = 8;
             // 
             // lblFechaDeVencimiento
             // 
             this.lblFechaDeVencimiento.AutoSize = true;
             this.lblFechaDeVencimiento.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblFechaDeVencimiento.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblFechaDeVencimiento.Location = new System.Drawing.Point(15, 212);
+            this.lblFechaDeVencimiento.Location = new System.Drawing.Point(15, 304);
+            this.lblFechaDeVencimiento.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFechaDeVencimiento.Name = "lblFechaDeVencimiento";
-            this.lblFechaDeVencimiento.Size = new System.Drawing.Size(122, 16);
+            this.lblFechaDeVencimiento.Size = new System.Drawing.Size(161, 22);
             this.lblFechaDeVencimiento.TabIndex = 9;
             this.lblFechaDeVencimiento.Text = "Fecha de vencimiento";
             // 
-            // mrcCategoria
+            // mrcCategorias
             // 
-            this.mrcCategoria.BackColor = System.Drawing.Color.Gainsboro;
-            this.mrcCategoria.Controls.Add(this.lstProducto);
-            this.mrcCategoria.Controls.Add(this.lblProducto);
-            this.mrcCategoria.Controls.Add(this.lblTipo);
-            this.mrcCategoria.Controls.Add(this.dateTimePicker1);
-            this.mrcCategoria.Controls.Add(this.lblFechaDeVencimiento);
-            this.mrcCategoria.Controls.Add(this.lstTipo);
-            this.mrcCategoria.Controls.Add(this.lblNombre);
-            this.mrcCategoria.Controls.Add(this.checkBox1);
-            this.mrcCategoria.Controls.Add(this.lstNombre);
-            this.mrcCategoria.Controls.Add(this.lstCantidad);
-            this.mrcCategoria.Controls.Add(this.lblCantidad);
-            this.mrcCategoria.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mrcCategoria.ForeColor = System.Drawing.Color.SteelBlue;
-            this.mrcCategoria.Location = new System.Drawing.Point(36, 29);
-            this.mrcCategoria.Name = "mrcCategoria";
-            this.mrcCategoria.Size = new System.Drawing.Size(356, 326);
-            this.mrcCategoria.TabIndex = 10;
-            this.mrcCategoria.TabStop = false;
-            this.mrcCategoria.Text = "Categoria";
+            this.mrcCategorias.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.mrcCategorias.Controls.Add(this.lstProducto);
+            this.mrcCategorias.Controls.Add(this.lblProducto);
+            this.mrcCategorias.Controls.Add(this.lblTipo);
+            this.mrcCategorias.Controls.Add(this.lstFechaDeVencimiento);
+            this.mrcCategorias.Controls.Add(this.lblFechaDeVencimiento);
+            this.mrcCategorias.Controls.Add(this.lstTipo);
+            this.mrcCategorias.Controls.Add(this.lblNombre);
+            this.mrcCategorias.Controls.Add(this.chkCaduca);
+            this.mrcCategorias.Controls.Add(this.lstNombre);
+            this.mrcCategorias.Controls.Add(this.lstCantidad);
+            this.mrcCategorias.Controls.Add(this.lblCantidad);
+            this.mrcCategorias.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mrcCategorias.ForeColor = System.Drawing.Color.Peru;
+            this.mrcCategorias.Location = new System.Drawing.Point(22, 13);
+            this.mrcCategorias.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mrcCategorias.Name = "mrcCategorias";
+            this.mrcCategorias.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mrcCategorias.Size = new System.Drawing.Size(448, 353);
+            this.mrcCategorias.TabIndex = 10;
+            this.mrcCategorias.TabStop = false;
+            this.mrcCategorias.Text = "Categorias";
+            // 
+            // lstProducto
+            // 
+            this.lstProducto.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstProducto.FormattingEnabled = true;
+            this.lstProducto.Items.AddRange(new object[] {
+            "Comida",
+            "Limpieza",
+            "Medicamento",
+            "Otro"});
+            this.lstProducto.Location = new System.Drawing.Point(97, 47);
+            this.lstProducto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstProducto.Name = "lstProducto";
+            this.lstProducto.Size = new System.Drawing.Size(160, 28);
+            this.lstProducto.TabIndex = 11;
             // 
             // lblProducto
             // 
             this.lblProducto.AutoSize = true;
             this.lblProducto.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblProducto.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblProducto.Location = new System.Drawing.Point(19, 29);
+            this.lblProducto.Location = new System.Drawing.Point(15, 47);
+            this.lblProducto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblProducto.Name = "lblProducto";
-            this.lblProducto.Size = new System.Drawing.Size(56, 16);
+            this.lblProducto.Size = new System.Drawing.Size(74, 22);
             this.lblProducto.TabIndex = 10;
             this.lblProducto.Text = "Producto";
             // 
-            // lstProducto
-            // 
-            this.lstProducto.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lstProducto.FormattingEnabled = true;
-            this.lstProducto.Location = new System.Drawing.Point(89, 26);
-            this.lstProducto.Name = "lstProducto";
-            this.lstProducto.Size = new System.Drawing.Size(121, 24);
-            this.lstProducto.TabIndex = 11;
-            // 
             // cmdGuardar
             // 
-            this.cmdGuardar.Location = new System.Drawing.Point(274, 388);
+            this.cmdGuardar.BackColor = System.Drawing.Color.White;
+            this.cmdGuardar.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdGuardar.Location = new System.Drawing.Point(209, 394);
+            this.cmdGuardar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmdGuardar.Name = "cmdGuardar";
-            this.cmdGuardar.Size = new System.Drawing.Size(75, 23);
+            this.cmdGuardar.Size = new System.Drawing.Size(116, 38);
             this.cmdGuardar.TabIndex = 11;
             this.cmdGuardar.Text = "GUARDAR";
-            this.cmdGuardar.UseVisualStyleBackColor = true;
+            this.cmdGuardar.UseVisualStyleBackColor = false;
             // 
             // cmdCancelar
             // 
-            this.cmdCancelar.Location = new System.Drawing.Point(377, 388);
+            this.cmdCancelar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.cmdCancelar.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdCancelar.Location = new System.Drawing.Point(333, 394);
+            this.cmdCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmdCancelar.Name = "cmdCancelar";
-            this.cmdCancelar.Size = new System.Drawing.Size(75, 23);
+            this.cmdCancelar.Size = new System.Drawing.Size(116, 38);
             this.cmdCancelar.TabIndex = 12;
             this.cmdCancelar.Text = "CANCELAR";
-            this.cmdCancelar.UseVisualStyleBackColor = true;
+            this.cmdCancelar.UseVisualStyleBackColor = false;
             // 
             // frmAlacena
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 526);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(490, 454);
             this.Controls.Add(this.cmdCancelar);
             this.Controls.Add(this.cmdGuardar);
-            this.Controls.Add(this.mrcCategoria);
+            this.Controls.Add(this.mrcCategorias);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmAlacena";
             this.Text = "Alacena";
-            this.mrcCategoria.ResumeLayout(false);
-            this.mrcCategoria.PerformLayout();
+            this.mrcCategorias.ResumeLayout(false);
+            this.mrcCategorias.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -221,10 +287,10 @@
         private System.Windows.Forms.ComboBox lstTipo;
         private System.Windows.Forms.ComboBox lstCantidad;
         private System.Windows.Forms.ComboBox lstNombre;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox chkCaduca;
+        private System.Windows.Forms.DateTimePicker lstFechaDeVencimiento;
         private System.Windows.Forms.Label lblFechaDeVencimiento;
-        private System.Windows.Forms.GroupBox mrcCategoria;
+        private System.Windows.Forms.GroupBox mrcCategorias;
         private System.Windows.Forms.ComboBox lstProducto;
         private System.Windows.Forms.Label lblProducto;
         private System.Windows.Forms.Button cmdGuardar;
--- pryMiPrimerApp/frmPrincipal.Designer.cs
+++ pryMiPrimerApp/frmPrincipal.Designer.cs
@@ -28,37 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.cmdAlacena = new System.Windows.Forms.Button();
+            this.cmdFinanzas = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // cmdAlacena
             // 
-            this.button1.Location = new System.Drawing.Point(62, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(514, 161);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "ALACENA";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.cmdAlacena.BackColor = System.Drawing.Color.BurlyWood;
+            this.cmdAlacena.Font = new System.Drawing.Font("Baby Pumpkin", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAlacena.Location = new System.Drawing.Point(13, 29);
+            this.cmdAlacena.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdAlacena.Name = "cmdAlacena";
+            this.cmdAlacena.Size = new System.Drawing.Size(566, 198);
+            this.cmdAlacena.TabIndex = 0;
+            this.cmdAlacena.Text = "ALACENA";
+            this.cmdAlacena.UseVisualStyleBackColor = false;
+            this.cmdAlacena.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // cmdFinanzas
             // 
-            this.button2.Location = new System.Drawing.Point(62, 217);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(514, 170);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "FINANZAS";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.cmdFinanzas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cmdFinanzas.Font = new System.Drawing.Font("Baby Pumpkin", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdFinanzas.Location = new System.Drawing.Point(13, 256);
+            this.cmdFinanzas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdFinanzas.Name = "cmdFinanzas";
+            this.cmdFinanzas.Size = new System.Drawing.Size(566, 198);
+            this.cmdFinanzas.TabIndex = 1;
+            this.cmdFinanzas.Text = "FINANZAS";
+            this.cmdFinanzas.UseVisualStyleBackColor = false;
+            this.cmdFinanzas.Click += new System.EventHandler(this.button2_Click);
             // 
             // frmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(617, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(594, 478);
+            this.Controls.Add(this.cmdFinanzas);
+            this.Controls.Add(this.cmdAlacena);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmPrincipal";
             this.Text = "frmPrincipal";
             this.ResumeLayout(false);
@@ -67,7 +74,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button cmdAlacena;
+        private System.Windows.Forms.Button cmdFinanzas;
     }
 }
